d31773d351eb5834608b2718e86e2e8b01c9129a
PaperCypress <linkzelda.brian@gmail.com>
2023-06-17 20:13:26 +0800

    617b

--- js/forumlogin.js
+++ js/forumlogin.js
@@ -1,19 +1,23 @@
-var userlist=[];
+    var usersloaded=false;
+    var userlist=[];
 	  var passlist=[];
 	  var userIDlist=[];
       var messageCount=[];
+      var userform=document.getElementById("user");
+      var passform=document.getElementById("pass");
       function getusers(){
         jQuery.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1OBU38M_mZylTzT3GjWrJsnOSySeQEgMcZhE7ujj-Vic/values/accounts/?key=AIzaSyCSGFd1PdXyieRtd8FXsgFZVyMnI3Q8Xkk", function (data) {
             var sheetData = data.values;
             for (i = 1; i < sheetData.length; i++) {
-            userlist[i-1]=sheetData[i][0]; 		
+            userlist[i-1]=sheetData[i][0]; 	
+            
             passlist[i-1]=sheetData[i][1]; 
             userIDlist[i-1]=sheetData[i][2]; 
             messageCount[i-1]=sheetData[i][4];
-            console.log(userIDlist[i-1]);
             }
             
             whenusersloaded();
+            usersloaded=true;
         });
         //
     }
@@ -23,13 +27,13 @@ $( document ).ready(function() {
     
     });
 function whenusersloaded(){
-    console.log(userIDlist[0]);
+    
+    //console.log(userIDlist[0]);
     if(Cookies.get("signedin")!=undefined){
         var index=userIDlist.findIndex(checkID);
         document.getElementById("loginform").style.display = 'none';
         document.getElementById("um").style.display = 'block';
-        document.getElementById("username").innerHTML=userlist[index];
-        
+        document.getElementById("username").innerHTML = userlist[index];
     }
     else{
         document.getElementById("loginform").style.display = 'block';
@@ -39,11 +43,29 @@ function whenusersloaded(){
 function logout(){
 	Cookies.remove('signedin');
     window.location.href="forum.html";
-
 }
 function login(){
-    
+    if(userlist!=undefined){
+			
+        if(userlist.findIndex(checkName)!=-1){
+            var index=userlist.findIndex(checkName);
+            if(passform.value==passlist[index]){
+                Cookies.set('signedin', userIDlist[index], { expires: 7 })
+                window.location.href="forum.html";
+            }
+            else{
+                window.location.href="error.html?wrongpw";
+            }
+            
+        }
+        else{
+            window.location.href="error.html?nouser";
+        }
+    }
 }
+function checkName(name) {
+    return name==userform.value;
+  }
 function checkID(id) {
     return id==Cookies.get("signedin");
   }
